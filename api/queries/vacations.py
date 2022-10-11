daac223222821744dc0da97616059313f0ae43e6
Greg Robertson <grobertson743@gmail.com>
2022-10-11 11:30:59 -0700

    more comments and added repository class/ repo patterns

--- api/queries/vacations.py
+++ api/queries/vacations.py
@@ -1,6 +1,6 @@
-from optparse import Option
 from pydantic import BaseModel
 from typing import Optional
+from datetime import date
 
 #Has nothing to do to with the database!!!!!!
 #(FastAPI) takes JSON and turns it into object
@@ -8,7 +8,7 @@ class VacationIn(BaseModel):
     # Unprocessable Entity Error: 
     # missing REQUIRED class properties
     name: str
-    from_date: str
-    to_date: str
+    from_date: date
+    to_date: date
     #Not Required (put Optional['type'])
     thoughts: Optional[str]
